3f4128510c97ca3431f6bd7d7d502df4f70ce3b1
Quinn Le <quynhnle135@gmail.com>
2023-05-29 12:15:37 -0400

    Did exercise 1. Hello World

---
+++ exercise_1/hello_world.py
@@ -0,0 +1,4 @@
+def print_helloworld():
+    print("Hello, world!")
+    name = input("What is your name? \n")
+    print(f"Hello, {name}")
---
+++ exercise_1/main.py
@@ -0,0 +1,9 @@
+from hello_world import print_helloworld
+
+
+def main():
+    print_helloworld()
+
+
+if __name__ == "__main__":
+    main()
