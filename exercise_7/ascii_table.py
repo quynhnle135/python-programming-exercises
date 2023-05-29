4264d8d4ef04d632748c9d7070fee7d2341b0e9b
Quinn Le <quynhnle135@gmail.com>
2023-05-29 17:42:47 -0400

    Did exercise 7. Ascii table

---
+++ exercise_7/ascii_table.py
@@ -0,0 +1,3 @@
+def print_ascii_table():
+    for i in range(32, 127):
+        print(f"{i}: {chr(i)}")
---
+++ exercise_7/main.py
@@ -0,0 +1,9 @@
+from ascii_table import print_ascii_table
+
+
+def main():
+    print_ascii_table()
+
+
+if __name__ == "__main__":
+    main()
