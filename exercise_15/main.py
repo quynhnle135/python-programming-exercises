50c7e1feabb77f3294e073dbde279564b214759c
Quinn Le <quynhnle135@gmail.com>
2023-06-01 15:45:40 -0400

    Updated

--- exercise_15/main.py
+++ exercise_15/main.py
@@ -3,7 +3,7 @@ from median import find_median
 
 def main():
     print(find_median([]))
-    print(find_median([1, 2, 3])
+    print(find_median([1, 2, 3]))
     print(find_median([1, 2, 3, 4]))
     print(find_median([3, 7, 10, 4, 1, 9, 6, 5, 2, 8]))
     print(find_median([3, 7, 10, 4, 1, 9, 6, 2, 8]))
