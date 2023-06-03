a2b98031cf050af2740705e8c1613cd5b22d6bd9
Quinn Le <quynhnle135@gmail.com>
2023-06-03 11:18:43 -0400

    Updated isLeapYear function

--- exercise_20/leap_year.py
+++ exercise_20/leap_year.py
@@ -1,6 +1,9 @@
 def isLeapYear(year) -> bool:
     if year % 400 == 0:
         return True
-    if year % 4 == 0 and year % 100 != 0:
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
         return True
-    return False
+    else:
+        return False
