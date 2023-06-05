3a8a5da60b7bb85da1b642b35f3c6be7348c81ee
Quinn Le <quynhnle135@gmail.com>
2023-06-05 13:34:53 -0400

    Added suggested solution

--- exercise_28/border_drawing.py
+++ exercise_28/border_drawing.py
@@ -1,3 +1,4 @@
+# my solution
 def draw_border(width, height):
     if width == 1 or height == 1:
         return
@@ -5,13 +6,22 @@ def draw_border(width, height):
     for row in range(height):
         for column in range(width):
             if (row == 0 and column == 0) or (row == 0 and column == width - 1) or (row == height - 1 and column == 0) or (row == height - 1 and column == width - 1):
-                print("+", end=" ")
+                print("+", end="")
             elif row == 0 or row == height - 1:
-                print("-", end=" ")
+                print("-", end="")
             elif column == 0 or column == width - 1:
-                print("|", end=" ")
+                print("|", end="")
             else:
-                print(" ", end=" ")
+                print(" ", end="")
         print()
 
+
+# suggested solution
+def drawBorder(width, height):
+    if width < 2 or height < 2:
+        return
+    print("+" + ("-" * (width - 2)) + "+")
+    for i in range(1, height - 1):
+        print("|" + " " * (width - 2) + "|")
+    print("+" + ("-" * (width - 2)) + "+")
 
--- exercise_28/main.py
+++ exercise_28/main.py
@@ -1,10 +1,14 @@
-from border_drawing import draw_border
+from border_drawing import draw_border, drawBorder
 
 
 def main():
     draw_border(16, 4)
+    print("USING SUGGESTED SOLUTION")
+    drawBorder(16, 4)
     print("NEXT DRAWING")
     draw_border(10, 5)
+    print("USING SUGGESTED SOLUTION")
+    drawBorder(10, 5)
 
 
 if __name__ == "__main__":
